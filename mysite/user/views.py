df6c1933ac05b61262110d1dad7003fa9dabec4f
ivan <ivan.chvartatskyy@gmail.com>
2021-04-08 10:44:15 +0300

    adding new changes

--- mysite/user/views.py
+++ mysite/user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
 
 
@@ -17,5 +18,6 @@ def register(request):
     return render(request, 'user/register.html', context)
 
 
+@login_required
 def profile(request):
     return render(request, 'user/profile.html')
